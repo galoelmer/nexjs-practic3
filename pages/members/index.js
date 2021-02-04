51c5b4282a663862ff2bc3828a57530d1b6cfc11
Elmer C. Galo <elmer.galo@outlook.com>
2021-02-04 01:18:04 -0500

    Add dynamic route

--- pages/members/index.js
+++ pages/members/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from '../../styles/Members.module.css';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -21,11 +22,11 @@ const Index = ({ members }) => {
       <div>
         <h1>Members</h1>
         {members.map((user) => (
-          <div key={user.id}>
+          <Link href={`/members/${user.id}`} key={user.id}>
             <a className={styles.single}>
               <h3>{user.name}</h3>
             </a>
-          </div>
+          </Link>
         ))}
       </div>
     </>
